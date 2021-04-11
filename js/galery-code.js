956332b610cf07d8c5da2f22ca982ff131dfcc11
OlegOghiroc <olegoghiroc@gmail.com>
2021-04-11 22:58:14 +0100

    зробив 8 дз

---
+++ js/galery-code.js
@@ -0,0 +1,70 @@
+import images from './gallery-items.js';
+
+
+const onGaleryList = document.querySelector('.js-gallery');
+const onLightboxOpenModal = document.querySelector('.lightbox');
+const closeLightboxButon = document.querySelector('[data-action="close-lightbox"]');
+const lightboxImageElement = document.querySelector('.lightbox__image');
+const overlayEl = document.querySelector('.lightbox__overlay');
+
+// ! Шаблон розмітки
+const lightboxImage = images.map((image) => {
+    return  `<li class="gallery__item">
+            <a
+                class="gallery__link"
+                href="${image.original}">
+                <img
+                    class="gallery__image"
+                    src="${image.preview}"
+                    data-source="${image.original}"
+                    alt="${image.description}"
+                />
+            </a>
+            </li>`;
+}).join('')
+
+onGaleryList.insertAdjacentHTML('afterbegin', lightboxImage);
+
+// ! Делегирование
+onGaleryList.addEventListener('click', onLightboxConteinerClick)
+function onLightboxConteinerClick(evt) {
+    
+    evt.preventDefault();
+
+    if (!evt.target.classList.contains('gallery__image')) {
+        return;
+    }
+    
+    onLightboxSwitchOnModal();
+    const currentSource = evt.target.dataset.source;
+    lightboxImageElement.src = currentSource;
+}
+
+// ! Відкриття модалки
+function onLightboxSwitchOnModal() {
+    window.addEventListener('keydown', onEscClose);
+    onLightboxOpenModal.classList.add('is-open');
+}
+
+
+// ! Закриття модалки
+closeLightboxButon.addEventListener('click', onLightboxCloseModal);
+function onLightboxCloseModal() {
+    window.removeEventListener('keydown', onEscClose);
+    onLightboxOpenModal.classList.remove('is-open');
+    lightboxImageElement.src = '';
+}
+
+// ! Закриття по оверлею
+overlayEl.addEventListener('click',onOverlayClose);
+function onOverlayClose(evt) {
+    onLightboxCloseModal()
+}
+
+// ! закриття по Esc
+function onEscClose(evt) {
+    console.log(evt.code)
+    if (evt.code === 'Escape') {
+        onLightboxCloseModal()
+    }
+}
